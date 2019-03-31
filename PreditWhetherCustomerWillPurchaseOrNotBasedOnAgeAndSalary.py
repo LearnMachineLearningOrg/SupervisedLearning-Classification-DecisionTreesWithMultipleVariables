6776e19728757bc25801fabff026c9870147c1a1
Venkata Raju Bankapalli <raju.intrepid@gmail.com>
2019-04-01 00:50:27 +0530

    Update PreditWhetherCustomerWillPurchaseOrNotBasedOnAgeAndSalary.py

--- PreditWhetherCustomerWillPurchaseOrNotBasedOnAgeAndSalary.py
+++ PreditWhetherCustomerWillPurchaseOrNotBasedOnAgeAndSalary.py
@@ -10,13 +10,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#This function is used to load CSV file from the 'data' directory 
+#in the present working directly 
+def loadCSV (fileName):
+    scriptDirectory = os.path.dirname(__file__)
+    dataDirectoryPath = "."
+    dataDirectory = os.path.join(scriptDirectory, dataDirectoryPath)
+    dataFilePath = os.path.join(dataDirectory, fileName)
+    return pd.read_csv(dataFilePath)
+
+#This funtion is used to preview the data in the given dataset
+def previewData (dataSet):
+    print(dataSet.head())
+    print("\n")
+
+#This function is used to check for missing values in a given dataSet
+def checkForMissingValues (dataSet):
+    print(dataSet.isnull().sum())
+    print("\n")
+
+#This function is used to check the statistics of a given dataSet
+def getStatisticsOfData (dataSet):
+    print("***** Datatype of each column in the data set: *****")
+    dataSet.info()
+    print("\n")
+    print("***** Columns in the data set: *****")
+    print(dataSet.columns.values)
+    print("***** Details about the data set: *****")
+    print(dataSet.describe())
+    print("\n")
+    print("***** Checking for any missing values in the data set: *****")
+    checkForMissingValues(dataSet)
+    print("\n")
+    
+#This funtion is used to handle the missing value in the features, in the 
+#given examples
+def handleMissingValues (features):
+    from sklearn.impute import SimpleImputer
+    imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+    imputer.fit(features)
+    imputedFeatures = imputer.fit_transform(features)
+    return imputedFeatures
+ 
 # Importing the dataset
 dataset = pd.read_csv('social_network_ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
+#Preview the dataSet and look at the statistics of the dataSet
+#Check for any missing values 
+#so that we will know whether to handle the missing values or not
+print("***** Preview the dataSet and look at the statistics of the dataSet *****")
+previewData(dataSet)
+getStatisticsOfData(dataSet)
+
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
